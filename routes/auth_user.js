5d86a2310d98f02700ecb190ae4d9636b4f9bc71
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-08-13 22:03:14 +0100

    Code Refactor : Create Auth Controller

--- routes/auth_user.js
+++ routes/auth_user.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router= express.Router();
-const {User, validateRegisterUser, validateLoginUser} = require("../models/User_model"); 
-const asyncHandler= require("express-async-handler");
-const {hashPassword , comparePasswords} = require("../Helper/password_hashing");
 const {blockNonAdminFromSettingIsAdmin}=require("../middlewares/verify_token");
+const {registerUser,loginUser} = require("../controllers/auth_controller");
+
 
 /* 
     * @desc Register New User 
@@ -11,71 +10,14 @@ const {blockNonAdminFromSettingIsAdmin}=require("../middlewares/verify_token");
     * @method POST
     * @acces Public
 */
-
-router.post("/register",blockNonAdminFromSettingIsAdmin,asyncHandler(
-   async (req,res)=>{
-    const { error } = validateRegisterUser(req.body);
-    if (error) {
-      return res.status(400).json({ message: error.details[0].message });
-    }
-    let user = await User.findOne({email : req.body.email});
-    if (user) {
-        res.status(400).json({
-            message : "User Already Exists"
-        });
-        return;
-    }
-    req.body.password=await  hashPassword(req.body.password); 
-    const newUser=new User({
-      userName:req.body.userName,
-      email:req.body.email,
-      password:req.body.password,
-
-    });
-    const result = await newUser.save();
-     const token = newUser.generateToken();
-    const {password, ...others}= result._doc;
-    res.status(201).json({...others , token : token});
-
-    }
-));
-
-
-
-
+router.post("/register",blockNonAdminFromSettingIsAdmin,registerUser);
 /* 
     * @desc Login New User 
     * @route /api/auth/login
     * @method POST
     * @acces Public
 */
-
-router.post("/login",blockNonAdminFromSettingIsAdmin,asyncHandler(
-   async (req,res)=>{
-    const { error } = validateLoginUser(req.body);
-    if (error) {
-      return res.status(400).json({ message: error.details[0].message });
-    }
-    let user = await User.findOne({email : req.body.email});
-    if (!user) {
-        res.status(400).json({
-            message : "Email Or Password Are Incorrect"
-        });
-        return;
-    } 
-    const isPasswordMatch=await  comparePasswords(req.body.password,user.password); 
-    if (!isPasswordMatch) {
-                res.status(400).json({
-            message : "Email Or Password Are Incorrect"
-        });
-        return; 
-    }
-    const token = user.generateToken();
-     const { password, ...others } = user._doc;
-     res.status(200).json({...others , token : token});
-
-    }
-));
+router.post("/login",blockNonAdminFromSettingIsAdmin,loginUser);
  
 
 module.exports = router
